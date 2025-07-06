0f4e9f2f0dad02eb077afd394665c9097bedd6b6
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-07-06 09:41:08 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_info = command.split()
+    if len(command_info) != 3:
+        return None
+    elif command_info[0] != "cp":
+        return None
+    file_in_name = command_info[1]
+    file_out_name = command_info[2]
+    try:
+        with (open(file_in_name, "r") as file_in,
+              open(file_out_name, "w") as file_out):
+            line = file_in.readline()
+            file_out.write(line)
+    except FileNotFoundError as e:
+        print("Some of files is missing!", e)
